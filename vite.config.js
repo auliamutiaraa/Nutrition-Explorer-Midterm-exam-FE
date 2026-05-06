d01dd014e316289bcbecc7e22e224d74429baab0
Ridho <ridho@example.com>
2026-05-06 21:05:57 +0700

    build: use stable asset filenames

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/index.js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name][extname]",
+      },
+    },
+  },
 });
